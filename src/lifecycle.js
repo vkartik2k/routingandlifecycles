c0c849e5acbdc388bbc5e9c68ea235a6ad9399b4
vkartik2k <vkartik2k@gmail.com>
2019-04-14 15:25:56 +0530

    Update lifecycle.js

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,28 +1,11 @@
 import React, { PureComponent } from 'react';
-
 class Life extends PureComponent {
-    // 1 get default props
-
-    // 2 set default state
     state = {
         title:'Life cycles'
     }
-
-    // 5 after render
-    componentDidMount(){
-        console.log('5 after render')
-        document.querySelector('h3').style.color = 'red'
-    }
-
-    // 3 before render
-    componentWillMount(){
-        console.log('3 before render');
-    }
-
     componentWillUpdate(){
         console.log('BEFORE UPDATE')
     }
-
     componentDidUpdate(){
         console.log('AFTER UPDATE')
     }
@@ -32,18 +15,14 @@ class Life extends PureComponent {
         if(nextState.title === this.state.title){
             return false
         }
-
         return true;
     }
-
     componentWillReceiveProps(){
         console.log('BEFORE RECEIVE PROPS')
     }
-
     componentWillUnmount(){
         console.log('UNMOUNT')
     }
-
     // 4 render jxs
     render(){
         console.log('RENDER')
